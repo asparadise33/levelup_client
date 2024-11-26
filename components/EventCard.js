09fa1dd30912f84a966a0357b71c6fba80f21a17
asparadise33 <abby.paradise@gmail.com>
2024-11-25 21:32:30 -0600

    added form, create and get requests for games

--- components/EventCard.js
+++ components/EventCard.js
@@ -1,25 +1,49 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { propTypes } from 'react-bootstrap/esm/Image';
 
 const EventCard = ({
+  game,
   description,
   date,
   time,
+  organizer,
 }) => (
   <Card className="text-center">
-    <Card.Header>{description}</Card.Header>
+    <Card.Header>{game.title}</Card.Header>
     <Card.Body>
-      <Card.Text>{date}</Card.Text>
-      <Card.Text>{time}</Card.Text>
+      <Card.Text>{description}</Card.Text>
+      <Card.Text> On {date}</Card.Text>
+      <Card.Text> At {time} PM</Card.Text>
+      <Card.Footer className="text-muted">Organizer: {organizer.id}</Card.Footer>
     </Card.Body>
   </Card>
 );
 
 EventCard.propTypes = {
+  game: PropTypes.shape({
+    id: PropTypes.number,
+    game_type: PropTypes.shape({
+      label: propTypes.string,
+    }),
+    title: PropTypes.string,
+    maker: PropTypes.string,
+    gamer: PropTypes.shape({
+      uid: PropTypes.string,
+      bio: PropTypes.string,
+    }),
+    number_of_players: PropTypes.string,
+    skill_level: PropTypes.string,
+  }).isRequired,
   description: PropTypes.string.isRequired,
   date: PropTypes.instanceOf(Date).isRequired,
   time: PropTypes.instanceOf(Date).isRequired,
+  organizer: PropTypes.shape({
+    id: PropTypes.number,
+    uid: PropTypes.string,
+    bio: PropTypes.string,
+  }).isRequired,
 };
 
 export default EventCard;
